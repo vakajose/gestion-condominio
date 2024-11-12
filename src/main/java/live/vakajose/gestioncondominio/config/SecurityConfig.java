a22ba43cd1c5546b400061a94bce0a0fc5c0415c
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2024-11-12 19:13:11 -0400

    COnfig secutiry cors para localhost

--- src/main/java/live/vakajose/gestioncondominio/config/SecurityConfig.java
+++ src/main/java/live/vakajose/gestioncondominio/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -22,5 +24,17 @@ public class SecurityConfig {
                 )
                 .httpBasic(Customizer.withDefaults());  // Permite autenticación básica (opcional para pruebas)
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/graphql")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
     }
 }
